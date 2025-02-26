bbfd9b0fd59e15065bf5fb07ef3efb955c1fdba6
rulaaja <roope.rajala@metropolia.fi>
2025-02-26 15:13:00 +0200

    tehtävät

---
+++ kokeilut/liittymä.py
@@ -0,0 +1,17 @@
+
+print("""\
+
+   
+      __|__
+*---o--(_)--o---*                _  _
+                               ( `   )_
+   MIEHEN SALAINEN MATKA    (    )    `)
+      THAIMAA             (_   (_ .  _) _)                       
+    
+   """)
+aloitus=input("Paina mitä vain aloittaaksesi: ")
+print("Tehtävä: Olet kiireinen työmies, jolla on tärkeitä työasioita ulkomailla.")
+print("Haluat kuitenkin suorittaa äijäreissun Thaimaaseen töiden ohessa.")
+print("Sinun täytyy matkustaa Thaimaaseen ilman, että ylität CO2-budjettisi.")
+
+
